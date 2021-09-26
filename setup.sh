9e6d583b0364984afb49af5bb1dd969d77b66c6b
Ayhon <ayhonnick@gmail.com>
2021-09-26 22:35:33 +0200

    Refactoring and new data, fzf & keymapper configs

--- setup.sh
+++ setup.sh
@@ -10,10 +10,12 @@ DECAY_DIR="$HOME/Decay"
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 DOTFILES_DIR="$GIT_DIR/dotfiles"
 CMD_NAME="setup"
+DATA_DIR_NAME="data"
+SAVED_DIRECTORIES="Desktop Documents Downloads Music Pictures Videos"
 
 REQUIRED_PKGS="stow git curl"
-BASIC_PKGS="tmux neovim git unrar mpv pandoc tldr gdb feh"
-EXTRA_PKGS="obs gimp yad flameshot translate-shell ranger rofi"
+BASIC_PKGS="tmux neovim git unrar mpv pandoc tldr gdb fzf"
+EXTRA_PKGS="obs gimp yad flameshot translate-shell ranger rofi feh"
 
 # Giving information
 goal_msg(){ printf "\e[33;1m[GOAL]\e[m %s\n" "$@"; }
@@ -23,6 +25,28 @@ print_err (){ printf "\e[31;1m[ERROR]\e[m %s\n" "$@" >&2; }
 # Getting information
 get_display_server(){
 	loginctl show-session $(loginctl | grep $(whoami) | cut -d" " -f6) -p Type | cut -d= -f2
+}
+get_distro_id(){
+	head -n1 /etc/*-release | grep NAME | sed -s "s/NAME=//g;s/\!//g;s/\"//g"
+}
+get_shell(){
+	basename $SHELL
+}
+get_shell_RC(){
+	case "$(get_shell)" in
+		"zsh")
+			rc_shell="$HOME/.zshrc"
+			;;
+		"bash")
+			rc_shell="$HOME/.bashrc"
+			;;
+		*)
+			print_err "Couldn't recognize shell. Using .bashrc"
+			rc_shell="$HOME/.bashrc" # Default
+			return 1
+			;;
+	esac
+	echo $rc_shell
 }
 
 # Program installation
@@ -62,8 +86,7 @@ inst(){
 	if [[ "$OSTYPE" == "linux"* ]]; then
 		ensure_root
 
-		distro_id="$(head -n1 /etc/*-release | grep NAME)"
-		distro_id="$(echo $distro_id | sed -s "s/NAME=//g;s/\!//g;s/\"//g")"
+		local distro_id="$(get_distro_id)"
 		verbose_msg "Detected distribution: $distro_id"
 
 		case $distro_id in
@@ -94,33 +117,21 @@ inst(){
 
 }
 
-get_RC_shell(){
-	case "$(basename $SHELL)" in
-		"zsh")
-			rc_shell="$HOME/.zshrc"
-			;;
-		"bash")
-			rc_shell="$HOME/.bashrc"
-			;;
-		*)
-			print_err "Couldn't recognize shell. Using .bashrc"
-			rc_shell="$HOME/.bashrc" # Default
-			return 1
-			;;
-	esac
-	echo $rc_shell
-}
 
 install_script(){
 	# install_script {script-name}̣̣̣̣ [executable-name]
-	local script_pathname="$DOTFILES_DIR/scripts/$1" 
-	
-	[ -f "$script_pathname" ] || return 1
+	if which $2 >/dev/null 2>&1; then
+		verbose_msg "Executable $2 found in path"
+	else
+		local script_pathname="$DOTFILES_DIR/scripts/$1" 
 
-	chmod +x "$script_pathname"
+		[ -f "$script_pathname" ] || return 1
 
-	mkdir -p "$HOME/.local/bin"
-	ln -s "$script_pathname" "$HOME/.local/bin/$([ -z $2 ] && echo $1 || echo $2)"
+		chmod +x "$script_pathname"
+
+		mkdir -p "$HOME/.local/bin"
+		ln -s "$script_pathname" "$HOME/.local/bin/$([ -z $2 ] && echo $1 || echo $2)"
+	fi
 }
 
 # Main function
@@ -156,6 +167,9 @@ setup(){
 
 					touch "$HOME/.ssh/authorized_keys"
 					chmod 600 "$HOME/.ssh/authorized_keys"
+
+					touch "$HOME/.ssh/config"
+					chmod 600 "$HOME/.ssh/config"
 				fi
 			fi
 
@@ -167,7 +181,7 @@ setup(){
 		"vim")
 			goal_msg "Configuring vim..."
 			dependencies "vim"
-			stow -Sd $DOTFILES_DIR -t $HOME vim
+			stow -Svd $DOTFILES_DIR -t $HOME vim
 			[ -f "~/.vim/autoload/plug.vim" ] || curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 				    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 			;;
@@ -176,7 +190,7 @@ setup(){
 			goal_msg "Configuring nvim..."
 			setup "vim"
 			dependencies "neovim"
-			stow -Sd $DOTFILES_DIR -t $HOME nvim
+			stow -Svd $DOTFILES_DIR -t $HOME nvim
 			[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ] \
 				|| curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
 				--create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -187,11 +201,18 @@ setup(){
 			[ -d "$nvim_ultisnips_dir" ] && rmdir "$nvim_ultisnips_dir"
 			ln -s "$vim_ultisnips_dir" "$nvim_ultisnips_dir"
 
+			goal_msg "Enable after syntax files from vim"
+			local vim_after_syntax_dir="$DOTFILES_DIR/vim/.vim/after/syntax/" 
+			local nvim_after_syntax_dir="$DOTFILES_DIR/nvim/.config/nvim/after/syntax"
+			[ -d "$nvim_after_syntax_dir" ] && rmdir "$nvim_after_syntax_dir"
+			mkdir -p "$(dirname $nvim_after_syntax_dir)"
+			ln -s "$vim_after_syntax_dir" "$nvim_after_syntax_dir"
+
 			;;
 		"tmux")
 			goal_msg "Configuring tmux..."
 			dependencies "tmux"
-			stow -Sd $DOTFILES_DIR -t $HOME tmux
+			stow -Svd $DOTFILES_DIR -t $HOME tmux
 			;;
 
 		"basic")
@@ -203,6 +224,23 @@ setup(){
 			goal_msg "Configuring extra..."
 			dependencies "$BASIC_PKGS"
 			dependencies "$EXTRA_PKGS"
+			;;
+
+		"data")
+			goal_msg "Configuring data..."
+
+			goal_msg "Finding (or creating) $DATA_DIR_NAME"
+			local data_dir="$HOME/.local/share/$DATA_DIR_NAME"
+			if [ ! -d $data_dir ]; then
+				verbose_msg "$data_dir is not a directory"
+				for saved_dir in $SAVED_DIRECTORIES; do
+					mkdir -p "$data_dir/$saved_dir"
+				done
+			fi
+
+			goal_msg "Creating symlinks "
+			stow -Svd "$HOME/.local/share" -t "$HOME" "$DATA_DIR_NAME"
+
 			;;
 
 		"test")
@@ -232,12 +270,13 @@ setup(){
 		"colors")
 			echo 'for i in {0..9}; do 	
 				echo -e \"\e[3${i}mThis is 3$i\e[m";
-			done' >> $(get_RC_shell)
-
+			done' >> $(get_shell_RC)
 			;;
 
 		"gdrive")
 			dependencies "rclone"
+			print_err "fzf autocompletion still under construction"
+			return 1
 			install_script "sync_drive.sh"
 
 			# TODO:
@@ -259,6 +298,40 @@ setup(){
 			else
 				print_err "Provide a --target=DIRECTORY to the gdrive targets"
 			fi
+			;;
+		"fzf")
+			goal_msg "Configuring fzf"
+			dependencies "fzf ripgrep"
+
+			goal_msg "Configuring fzf keybindings"
+			local keybindings_dir="/usr/share/fzf/shell/key-bindings.$(get_shell)"
+ 	 		if [ -f  "$keybindings_dir" ]; then
+				echo "source $keybindings_dir" >> $(get_shell_RC)
+			fi
+
+			goal_msg "Using ripgrep for fzf instead of find"
+			echo "export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob \"!.git/*\"'" >> $(get_shell_RC)
+
+			goal_msg "Configuring fzf autocompletion"
+			;;
+		"keymapper")
+			print_err "fzf autocompletion still under construction"
+			return 1
+
+			dependencies "pip python3-setuptools python3-devel"
+
+			goal_msg "Downloading program"
+			sudo pip install git+https://github.com/sezanzeb/key-mapper.git
+
+			goal_msg "Enabling key-mapper.service"
+			sudo systemctl enable --now key-mapper
+			;;
+		"ssh")
+			print_err "Not implemented"
+			return 1
+
+			eval "$(ssh-agent -s)"
+			ssh-add "$HOME/.ssh/ub_slab"
 			;;
 		*)
 			print_err "Option not recognized: \"$1\""
@@ -283,6 +356,7 @@ Targets:
   vim    - Setup vim config
   nvim   - Setup nvim config
   decay  - Setup a decaying directory
+  fzf    - Setup fzf with shortcuts
 EOF
 }
 is_argument(){
