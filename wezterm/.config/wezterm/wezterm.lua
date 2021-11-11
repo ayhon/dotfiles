d2017dcf51785d7237cc07c8bdfeef6054763468
Ayhon <ayhonnick@gmail.com>
2021-11-11 15:46:53 +0100

    Added wezterm configuration

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,40 @@
+-- Documentation at https://wezfurlong.org/wezterm/index.html
+return {
+	enable_tab_bar = false,
+	colors = {
+		foreground = "#c2c2c2",
+		background = "#222222",
+
+		-- cursor_fg = "",
+		-- cursor_bg = "",
+
+		-- cursor_border = "",
+
+		-- selection_fg = "",
+		-- selection_bg = "",
+
+		-- scrollbar_thumb = "",
+
+		-- split = "",
+
+		ansi = {
+			"#171421", -- Black
+			"#ff2b3a", -- Red
+			"#00ff57", -- Green
+			"#f9cb1b", -- Yellow
+			"#2586ff", -- Dark blue
+			"#cc00ff", -- Purple
+			"#00ffe0", -- Cyan
+			"#c0c0b8", -- Grey
+		},
+		-- brights = {},
+		-- indexed = {},
+	},
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	}
+}
+-- vim : set fdm=marker :
